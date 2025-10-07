ace558cf7452eac52249763962bc5719dc857776
yrivek <yriveks1@gmail.com>
2025-10-07 16:16:09 +0200

    chore: initial commit

---
+++ backend/app/auth.py
@@ -0,0 +1,104 @@
+# para la expiracion del token
+from datetime import datetime, timedelta
+from typing import Annotated
+from fastapi import Depends, HTTPException, APIRouter
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+from app.database import SessionLocal
+from app.models.user import User
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+import os
+from dotenv import load_dotenv
+
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+)
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+class CreateUserRrequest(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
+    
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(db:db_dependency, create_user_request: CreateUserRrequest):
+    create_user_model = User(
+        username = create_user_request.username,
+        email = create_user_request.email, 
+        password = bcrypt_context.hash(create_user_request.password)
+    )
+    db.add(create_user_model)
+    db.commit()
+
+
+@router.post("/login", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], 
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    token = create_access_token(user.username, user.id, timedelta(minutes=30))
+    return{'access_token': token, 'token_type': 'bearer'}
+    
+def authenticate_user (username: str, password: str, db):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.password):
+        return False
+    return user
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id}
+    expire = datetime.utcnow() + expires_delta
+    encode.update({"exp": expire})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
---
+++ backend/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from typing import Annotated
+from app.database import engine, SessionLocal
+from app.models import user
+from sqlalchemy.orm import Session
+from  app import auth
+from app.auth import get_current_user
+
+app = FastAPI()
+app.include_router(auth.router)
+
+user.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK, tags=["user"])
+async def user(user:user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"User": user}
