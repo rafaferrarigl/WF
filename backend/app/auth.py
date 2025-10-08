302c662dfd4033616688f0f1f06d87769e990bda
yrivek <yriveks1@gmail.com>
2025-10-08 19:23:28 +0200

    token expiration check

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -89,6 +89,14 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
+        expire_date = payload.get("exp")
+        print(expire_date)
+        if datetime.utcnow() > datetime.fromtimestamp(expire_date):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token has expired",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
         if username is None or user_id is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
