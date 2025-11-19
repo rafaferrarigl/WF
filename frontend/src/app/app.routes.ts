0389f22948b3e89fd7fb74445f89b0377bc06778
Rafael Ferrari <yriveks1@gmail.com>
2025-11-19 21:40:51 +0100

    login and dashboard template

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {Login} from './login/login';
+import {Register} from './register/register';
+import {Dashboard} from './dashboard/dashboard';
+import {DashboardAdmin} from './dashboard-admin/dashboard-admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "Login", pathMatch: "full" },
+  {path: "Login", component: Login, pathMatch: "full"},
+  {path: "Register", component: Register, pathMatch: "full"},
+  {path: "dashboard", component: Dashboard, pathMatch: "full"},
+  {path: "dashboardadmin", component: DashboardAdmin, pathMatch: "full"}
+
+];
