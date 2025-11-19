7dbf65ee31c781a177bf3fac65c4e642b74d431c
Rafael Ferrari <yriveks1@gmail.com>
2025-11-19 21:41:02 +0100

    login and dashboard template

---
+++ frontend/src/app/login/login.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+
+  form: any;
+
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private fb: FormBuilder,
+  ){
+    this.form = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+  }
+
+
+
+  login() {
+    if (this.form.invalid) return;
+
+    const username = this.form.value.username!;
+    const password = this.form.value.password!;
+
+
+    this.auth.login(username, password).subscribe({
+      next: (res) => {
+
+        localStorage.setItem('token', res.access_token);
+
+
+        this.auth.me().subscribe({
+          next: (user: any) => {
+            console.log("Usuario autenticado:", user);
+
+            if (user.is_admin) {
+              this.router.navigate(['/dashboardadmin']);
+            } else {
+              this.router.navigate(['/dashboard']);
+            }
+          },
+          error: (err) => console.error("Error obteniendo usuario:", err)
+        });
+      },
+
+
+
+      error: (err) => console.error('Login fallo', err)
+
+
+    })
+  }
+}
---
+++ frontend/src/app/service/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+  private login_url = 'http://127.0.0.1:8443/auth/login';
+  private me_url = 'http://127.0.0.1:8443/auth/me';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    const body = new URLSearchParams();
+    body.set('username', username);
+    body.set('password', password);
+
+    return this.http.post(this.login_url, body.toString(), {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded'
+      })
+    });
+  }
+
+  me() {
+    const token = localStorage.getItem('token');
+
+    return this.http.get(this.me_url, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+}
