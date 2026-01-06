f8a5b07b9b3294c9d45998ec6d0d5f1d8b185407
Julio C. Galindo <julio.galindo@biologit.com>
2026-01-06 22:03:02 +0100

    Update OpenAPI generation script for better indentation and adjust Caddyfile for CSR handling

--- tools/generate-openapi.py
+++ tools/generate-openapi.py
@@ -1,18 +1,20 @@
 from __future__ import annotations
 
 from json import dump
+from pathlib import Path
 from subprocess import Popen
+from sys import executable
 
 from backend.app.__main__ import app
 
 
 if __name__ == '__main__':
     with open('openapi.yaml', 'w') as f:
-        dump(app.openapi(), f)
+        dump(app.openapi(), f, indent=4)
 
     Popen(
         [
-            'openapi-generator-cli',
+            Path(executable).parent / 'openapi-generator-cli',
             'generate',
             '-i',
             'openapi.yaml',
